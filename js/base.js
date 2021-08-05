d660e1a18c6861a4cd75ba5ac80ddb45a8bc4fd1
Maj <59331721+Majus1@users.noreply.github.com>
2021-08-05 21:02:26 +0200

    Added the first graph
    Will continue styling it to the specifications

--- js/base.js
+++ js/base.js
@@ -1,7 +1,35 @@
 // Variables
 const alertCancleButton = document.querySelector("#alert-cancle-button");
 const alertNotificationBar = document.querySelector("#alert-notification-bar");
+
 // THE BASE
 alertCancleButton.addEventListener("click", ()=> {
     alertNotificationBar.style.display="none";
+});
+
+// THE GRAPHS
+
+// Web Traffic graph
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', "S"],
+
+        datasets: [{
+            label: 'none',
+            data: [60, 120, 175, 125, 225, 200, 100],
+            backgroundColor: [
+                'rgba(0, 47, 47)'
+            ]
+        }]
+
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
 });
