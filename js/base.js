a1c164acb771e2f35c99321e1101cc74bed8b0ca
Maj <59331721+Majus1@users.noreply.github.com>
2021-08-09 22:18:23 +0200

    Added switch logic to the button tab bars

--- js/base.js
+++ js/base.js
@@ -170,14 +170,16 @@ function createMonthlyTrafficGraph() {
     });
 };
 
-// ::::: The daily traffic graph tabs :::::
+// ::::: The overall traffic graph tabs :::::
+const chronologicalTabBar = document.querySelector("#chronological-tab-bar");
 const hourylTab = document.querySelector("#hourly-tab");
 const dailylTab = document.querySelector("#daily-tab");
 const weeklylTab = document.querySelector("#weekly-tab");
 const monthlylTab = document.querySelector("#monthly-tab");
 hourylTab.focus();
 
-dailylTab.addEventListener("click", () => {
+chronologicalTabBar.addEventListener("click", (event) => {
+  
     /*
     // ::::: GENERATE Web Traffic graph 2 fnction :::::
 
@@ -185,12 +187,30 @@ dailylTab.addEventListener("click", () => {
     • It will do so by removing the curent graph in #graph-container.
     • And adding a new one.
     */
+
     let graphContainer = document.querySelector("#graph-container");
-    graphContainer.lastElementChild.remove()
+    
+    switch (event.target.id) {
+        // With event.target.id we can determine witch button was pressed.
+        case "hourly-tab":
+            console.log("you clicked the hourly");
+        break;
+        case "daily-tab":
+            console.log("you clicked the daily");
+        break;
+        case "weekly-tab":
+            console.log("you clicked the weekly");
+        break;
+        case "monthly-tab":
+            console.log("you clicked the monthly");
+        break;
+    };
+
+
+    graphContainer.lastElementChild.remove();
     graphContainer.innerHTML=`<canvas id="dailyTraffic" class="graph" width="400" height="200"></canvas>`;
     createDailyTrafficGraph();
     
     // You will create 3 additional graphs add a function that removes the curent graph
     // and adds the a new graph coresponding with the graph that was of witch the button was clicked.
 });
-
