4570bdae3ba09bce0d1d327232efb658f4abae51
Maj <59331721+Majus1@users.noreply.github.com>
2021-08-10 19:18:52 +0200

    Traffic graph added

--- js/base.js
+++ js/base.js
@@ -1,6 +1,14 @@
 // Variables
 const alertCancleButton = document.querySelector("#alert-cancle-button");
 const alertNotificationBar = document.querySelector("#alert-notification-bar");
+// ::::: The overall traffic graph tabs :::::
+const chronologicalTabBar = document.querySelector("#chronological-tab-bar");
+const hourylTab = document.querySelector("#hourly-tab");
+const dailylTab = document.querySelector("#daily-tab");
+const weeklylTab = document.querySelector("#weekly-tab");
+const monthlylTab = document.querySelector("#monthly-tab");
+hourylTab.focus();
+
 
 // THE BASE
 alertCancleButton.addEventListener("click", ()=> {
@@ -8,11 +16,7 @@ alertCancleButton.addEventListener("click", ()=> {
 });
 
 
-
 // THE GRAPHS
-
-
-
 // ::::: Daily Traffic graph :::::
 var ctx = document.getElementById('dailyTrafic');
 var dailyTraficChart = new Chart(ctx, {
@@ -45,34 +49,37 @@ var dailyTraficChart = new Chart(ctx, {
 });
 
 // ::::: Web Traffic graph 1 :::::
-var chartCanvas2 = document.querySelector("#hourlyTraffic");
-var webTrafficChart = new Chart(chartCanvas2, {
-    type: "line",
-    data: {
-        labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', "27-3", "4-10", "11-17", "18-24", "25-31"],
-
-        datasets: [{
-            label: "Houryl",
-            data : [1808, 912, 2413, 1272, 268, 1070, 2688, 2211, 1000, 1098, 424],
-            backgroundColor:['rgba(0, 47, 47, 0.7)'],
-            borderColor: "rgba(0, 47, 47, 0.7)",
-            fill: true
-        }
-    ]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+function createHourlyTrafficGraph() {
+    var chartCanvas2 = document.querySelector("#hourlyTraffic");
+    var webTrafficChart = new Chart(chartCanvas2, {
+        type: "line",
+        data: {
+            labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', "27-3", "4-10", "11-17", "18-24", "25-31"],
+    
+            datasets: [{
+                label: "Houryl",
+                data : [1808, 912, 2413, 1272, 268, 1070, 2688, 2211, 1000, 1098, 424],
+                backgroundColor:['rgba(0, 47, 47, 0.7)'],
+                borderColor: "rgba(0, 47, 47, 0.7)",
+                fill: true
             }
+        ]
         },
-        plugins: {
-            legend: {
-                display: false
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
             }
         }
-    }
-});
+    });
+};
+createHourlyTrafficGraph()
 
 // ::::: GENERATE Web Traffic graph 2 fnction :::::
 function createDailyTrafficGraph() {
@@ -148,7 +155,7 @@ function createMonthlyTrafficGraph() {
 
             datasets: [{
                 label: "Houryl",
-                data : [1190, 224, 875, 2135, 2483, 757, 730, 1201, 2392, 1040, 21040150],
+                data : [1190, 224, 875, 2135, 2483, 757, 730, 1201, 2392, 1040, 2840],
                 backgroundColor:['rgba(0, 47, 47, 0.7)'],
                 borderColor: "rgba(0, 47, 47, 0.7)",
                 fill: true
@@ -170,47 +177,38 @@ function createMonthlyTrafficGraph() {
     });
 };
 
-// ::::: The overall traffic graph tabs :::::
-const chronologicalTabBar = document.querySelector("#chronological-tab-bar");
-const hourylTab = document.querySelector("#hourly-tab");
-const dailylTab = document.querySelector("#daily-tab");
-const weeklylTab = document.querySelector("#weekly-tab");
-const monthlylTab = document.querySelector("#monthly-tab");
-hourylTab.focus();
-
+// ::::: GENERATE WEB TRAFFIC GRAPH FUNCTION :::::
+// • When clicked on a tab it will display its coresponding graph.
+// • It will do so by removing the curent graph in #graph-container.
+// • And adding a new one.
 chronologicalTabBar.addEventListener("click", (event) => {
-  
-    /*
-    // ::::: GENERATE Web Traffic graph 2 fnction :::::
-
-    • When clicked on a tab it will display its coresponding graph.
-    • It will do so by removing the curent graph in #graph-container.
-    • And adding a new one.
-    */
-
     let graphContainer = document.querySelector("#graph-container");
     
     switch (event.target.id) {
         // With event.target.id we can determine witch button was pressed.
         case "hourly-tab":
+            // Inserts the canvas element with the coresponding class
+            graphContainer.innerHTML=`<canvas id="hourlyTraffic" class="graph" width="400" height="200"></canvas>`;
+            createHourlyTrafficGraph();
             console.log("you clicked the hourly");
         break;
         case "daily-tab":
+            // Inserts the canvas element with the coresponding class
+            graphContainer.innerHTML=`<canvas id="dailyTraffic" class="graph" width="400" height="200"></canvas>`;
+            createDailyTrafficGraph();
             console.log("you clicked the daily");
         break;
         case "weekly-tab":
+            // Inserts the canvas element with the coresponding class
+            graphContainer.innerHTML=`<canvas id="weeklyTraffic" class="graph" width="400" height="200"></canvas>`;
+            createWeeklyTrafficGraph();
             console.log("you clicked the weekly");
         break;
         case "monthly-tab":
+            // Inserts the canvas element with the coresponding class
+            graphContainer.innerHTML=`<canvas id="monthlyTraffic" class="graph" width="400" height="200"></canvas>`;
+            createMonthlyTrafficGraph();
             console.log("you clicked the monthly");
         break;
     };
-
-
-    graphContainer.lastElementChild.remove();
-    graphContainer.innerHTML=`<canvas id="dailyTraffic" class="graph" width="400" height="200"></canvas>`;
-    createDailyTrafficGraph();
-    
-    // You will create 3 additional graphs add a function that removes the curent graph
-    // and adds the a new graph coresponding with the graph that was of witch the button was clicked.
 });
